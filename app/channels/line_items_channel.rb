651f799974ecdecce80b4a6c260ff01b33e870f6
Adrian Eka Sanjaya <eekkaaadrian@gmail.com>
2018-06-17 20:45:22 +0700

    second commit

---
+++ app/channels/line_items_channel.rb
@@ -0,0 +1,10 @@
+class LineItemsChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+	stream_from "line_items"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,20 @@
+class OrderMailer < ApplicationMailer
+  default from: 'Adrian Eka <eekkaaadrian@gmail.com>'
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.received.subject
+  #
+  def received(order)
+    @order = order
+	
+	mail to: order.email, subject: 'Pragmatic Store Order Confirmation'
+  end
+  #   en.order_mailer.shipped.subject
+  #
+  def shipped(order)
+    @order = order
+	
+	mail to: order.email, subject: 'Pragmatic Store Order Shipped'
+  end
+end
